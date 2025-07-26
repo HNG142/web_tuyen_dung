b2dc24829f4674651676c5ae1f381230d7991555
Quoc <hungqn24104@gmail.com>
2025-07-26 17:48:51 +0700

    Configure SQLAlchemy to use postgresql+psycopg_binary dialect

--- app/database.py
+++ app/database.py
@@ -1,18 +1,23 @@
 import os
 from sqlmodel import create_engine, SQLModel, Session
 from dotenv import load_dotenv
+from sqlalchemy.engine import URL 
 
-# Tải biến môi trường từ .env (chỉ cho môi trường phát triển cục bộ)
-# Trên Heroku, biến môi trường sẽ được Heroku cung cấp trực tiếp
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./recruitment.db")
 
-# Đối với SQLite
-# engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
+parsed_url = URL.create.from_string(DATABASE_URL)
+if parsed_url.drivername == "psycopg2": 
+    parsed_url.drivername = "psycopg_binary"
+elif parsed_url.drivername == "postgresql":
+    parsed_url.drivername = "postgresql+psycopg_binary" 
+elif parsed_url.drivername == "postgresql+psycopg":
+    parsed_url.drivername = "postgresql+psycopg_binary"
 
-# Đối với PostgreSQL (khuyến nghị trên Heroku)
-engine = create_engine(DATABASE_URL.replace("postgresql://", "postgresql+psycopg_binary://"), echo=True)
+
+engine = create_engine(parsed_url, echo=True) 
+# -----------------------------------------------------------
 
 def create_db_and_tables():
     print("Creating database and tables...")
